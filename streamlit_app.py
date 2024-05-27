429bd9948d16b6e23a2c098a6982efd94dfc2c0b
mvrcosp <marcosffpoleto@gmail.com>
2024-05-26 21:36:57 -0300

    adding py file

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="MP Personal Page", page_icon=":tada:", layout="wide")
+
+st.header("Oi, Eu sou o Marcos Poleto! :wave:")
+st.title("Analista de sistemas de Curitiba, Brasil")
+st.write("Acho fascinante as possibilidades de extrair importantes decisões a partir de dados crus históricos ou coletados em tempo real!")
+st.write("Estou construindo essa webpage como entrega final da disciplina de Infraestrutura Computacional 2 do curso DSBD da UFPR.")
+st.write("[Meu GitHub](https://github.com/mvrcosp)")
