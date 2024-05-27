e27174e934231651ab597bcdf0b111b50629494f
mvrcosp <marcosffpoleto@gmail.com>
2024-05-26 22:47:48 -0300

    image and other editing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,17 @@
+#imports
+from PIL import Image
 import streamlit as st
 
+#loading image
+img_marcosp = Image.open("images/marcosp.png")
+
+#page config
 st.set_page_config(page_title="MP Personal Page", page_icon=":tada:", layout="wide")
 
-st.header("Oi, Eu sou o Marcos Poleto! :wave:")
-st.title("Analista de sistemas de Curitiba, Brasil")
-st.write("Acho fascinante as possibilidades de extrair importantes decisões a partir de dados crus históricos ou coletados em tempo real!")
+# Introduction
+st.title("Oi, Eu sou o Marcos Poleto! :wave:")
+st.image(img_marcosp)
+st.subheader("Analista de sistemas e aluno da especialização em Data Science e Big Data da UFPR")
+st.write("Me interesso bastante no tratamento e manipulação de dados crus para gerar insights ou realizar previsões.")
 st.write("Estou construindo essa webpage como entrega final da disciplina de Infraestrutura Computacional 2 do curso DSBD da UFPR.")
-st.write("[Meu GitHub](https://github.com/mvrcosp)")
+st.write("[Meu GitHub >](https://github.com/mvrcosp)")
